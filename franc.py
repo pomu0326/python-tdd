596de7e90d7d5c50ce5075f2bb5bc04fea162d39
nomura.kota <nomura.kota@userside.co.jp>
2018-07-27 19:11:54 +0900

    Francクラスを作成する

---
+++ franc.py
@@ -0,0 +1,15 @@
+class Franc:
+    __amount = 0
+
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def times(self, multiplier):
+        return Franc(self.__amount * multiplier)
+
+    def equals(self, object):
+        return self.__amount == object.__amount
+
+    def __eq__(self, other):
+        return self.equals(other)
+
--- testmoney.py
+++ testmoney.py
@@ -1,11 +1,17 @@
 import unittest
 from doller import Doller
+from franc import Franc
 
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
         five = Doller(5)
         self.assertEqual(Doller(10), five.times(2))
         self.assertEqual(Doller(15), five.times(3))
+
+    def test_franc_multiplication(self):
+        five = Franc(5)
+        self.assertEqual(Franc(10), five.times(2))
+        self.assertEqual(Franc(15), five.times(3))
 
     def test_equality(self):
         self.assertTrue(Doller(5).equals(Doller(5)))
