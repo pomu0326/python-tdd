b612427031de584428555e88f7cd0c708b101700
nomura.kota <nomura.kota@userside.co.jp>
2018-11-17 11:24:03 +0900

    MoneyクラスにDollerを作るメソッドを追加する

--- money.py
+++ money.py
@@ -1,8 +1,11 @@
-from doller import Doller
+from abc import *
 
-class Money:
+class Money(metaclass=ABCMeta):
     def __init__(self, amount):
         self._amount = amount
+
+    @abstractmethod
+    def times(self, multiplier): pass
 
     def equals(self, object):
         if not isinstance(object, Money):
@@ -13,3 +16,14 @@ class Money:
 
     def doller(amount):
         return Doller(amount)
+
+class Doller(Money):
+
+    def __init__(self, amount):
+        super().__init__(amount)
+
+    def times(self, multiplier) -> Money:
+        return Doller(self._amount * multiplier)
+
+    def __eq__(self, other):
+        return self.equals(other)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,13 +1,12 @@
 import unittest
-from doller import Doller
 from franc import Franc
-from money import Money
+from money import Money, Doller
 
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
         five = Money.doller(5)
-        self.assertEqual(Doller(10), five.times(2))
-        self.assertEqual(Doller(15), five.times(3))
+        self.assertEqual(Money.doller(10), five.times(2))
+        self.assertEqual(Money.doller(15), five.times(3))
 
     def test_franc_multiplication(self):
         five = Franc(5)
@@ -15,11 +14,11 @@ class TestMoney(unittest.TestCase):
         self.assertEqual(Franc(15), five.times(3))
 
     def test_equality(self):
-        self.assertTrue(Doller(5).equals(Doller(5)))
-        self.assertFalse(Doller(5).equals(Doller(6)))
+        self.assertTrue(Money.doller(5).equals(Money.doller(5)))
+        self.assertFalse(Money.doller(5).equals(Money.doller(6)))
         self.assertTrue(Franc(5).equals(Franc(5)))
         self.assertFalse(Franc(5).equals(Franc(6)))
-        self.assertFalse(Franc(5).equals(Doller(5)))
+        self.assertFalse(Franc(5).equals(Money.doller(5)))
 
 if __name__ == "__main__":
     unittest.main()
